daaeb6b797466c6f009ed01aba39ecfac1ef7705
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2020-10-16 22:47:40 +0100

    Dev page

--- Startup.cs
+++ Startup.cs
@@ -46,7 +46,8 @@ namespace Illusive {
             if ( env.IsDevelopment() ) {
                 app.UseDeveloperExceptionPage();
             } else {
-                app.UseExceptionHandler("/Error");
+                app.UseDeveloperExceptionPage();
+                //app.UseExceptionHandler("/Error");
 
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
