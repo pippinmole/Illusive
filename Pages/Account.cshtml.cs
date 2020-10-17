b9da905015c768ec8855eceb034294e76de9c9a4
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2020-10-17 13:44:37 +0100

    Fixed logging

--- Pages/Account.cshtml.cs
+++ Pages/Account.cshtml.cs
@@ -1,36 +1,34 @@
 ﻿using System;
+using DnsClient.Internal;
 using Illusive.Illusive.Database.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace Illusive.Pages {
     public class AccountModel : PageModel {
+        
+        private readonly ILogger<AccountModel> _logger;
+        private readonly IAccountService _accountService;
 
-        private readonly IDatabaseContext databaseContext;
-        private readonly IAccountService accountService;
-
-        public AccountModel(IDatabaseContext dbContext, IAccountService accService) {
-            this.databaseContext = dbContext;
-            this.accountService = accService ?? throw new NullReferenceException("AccountService needs to be setup!");
+        public AccountModel(ILogger<AccountModel> logger, IAccountService accService) {
+            this._logger = logger;
+            this._accountService = accService ?? throw new NullReferenceException("AccountService needs to be setup!");
             
-            Console.WriteLine($"AccountService: {this.accountService}");
+            Console.WriteLine($"AccountService: {this._accountService}");
         }
         
         public IActionResult OnGet() {
             if ( !this.User.Identity.IsAuthenticated )
                 return this.RedirectToPage("/Index");
-
-            // Get account data
-            
-            Console.WriteLine("Account OnGet");
             
             return this.Page();
         }
 
         public string GetData() {
-            Console.WriteLine("Getting data for client");
+            this._logger.LogWarning($"Getting account details for email@email.com");
             
-            var account = this.accountService.GetAccount("email@email.com");
+            var account = this._accountService.GetAccount("email@email.com");
             if ( account != null ) {
                 return $"Account ID: {account.Id} \n" +
                        $"Account Email: {account.Email} \n" +
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,17 +5,15 @@ using Microsoft.Extensions.Logging;
 
 namespace Illusive.Pages {
     public class IndexModel : PageModel {
-        public IConfiguration Configuration { get; }
+        
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration) {
-            this.Configuration = configuration;
             this._logger = logger;
         }
 
         public void OnGet() {
-            this._logger.LogWarning($"Index: ConnString: {this.Configuration.GetConnectionString("AppConfig")}");
-            Console.WriteLine($"OnGet Index IsAuth: {this.User.Identity.IsAuthenticated}");
+            this._logger.LogWarning($"OnGet /Index");
         }
     }
 }
