d2baaca50cd1bad411e230286f4c5f803952dbad
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2020-10-31 21:28:38 +0000

    Fixed OwnerId being a required field in forum requests

--- Illusive.Database/Data/ForumData.cs
+++ Illusive.Database/Data/ForumData.cs
@@ -10,16 +10,14 @@ namespace Illusive.Illusive.Database.Models {
 
         [BsonId] public string Id { get; set; }
 
-        [Required] [StringLength(80)] public string Title { get; set; }
+        [Required, StringLength(80)] public string Title { get; set; }
 
         [Required, MaxLength(10000)] public string Content { get; set; }
 
         [MaxLength(100)] public string? Tags { get; set; } = "";
         
-        public string OwnerId { get; set; }
-
+        public string? OwnerId { get; set; } // Will be null in the user-request, but not when stored in the database.
         public DateTime TimeCreated { get; set;  }
-
         public List<ForumReply> Replies { get; set; }
         public bool IsMarkdown { get; set; }
         public uint Views { get; set; }
