18266f998aa329da028a38e6b309971ca57db439
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2020-10-31 03:07:52 +0000

    Added various layout elements
    + Forum posts now display the forum owner's profile picture
    + Account owners can now toggle an 'edit profile' panel
    +

--- Pages/Account.cshtml.cs
+++ Pages/Account.cshtml.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Illusive.Illusive.Cdn.Interfaces;
+using Illusive.Illusive.Data;
 using Illusive.Illusive.Database.Interfaces;
 using Illusive.Illusive.Database.Models;
 using Microsoft.AspNetCore.Http;
@@ -21,17 +21,14 @@ namespace Illusive.Pages {
         private readonly IAccountService _accountService;
         private readonly IContentService _contentService;
         
-        private static readonly List<string> ImageExtensions = new List<string> { ".JPG", ".JPE", ".BMP", ".GIF", ".PNG" };
-        private static readonly long MaxFileSizeBytes = 5 * 1000 * 1000;
+        [BindProperty] public AccountChangeData AccountUpdate { get; set; }
         
-        public IFormFile FileUpload { get; set; }
-
         public AccountModel(ILogger<AccountModel> logger, IAccountService accService, IContentService contentService) {
             this._logger = logger;
             this._accountService = accService ?? throw new NullReferenceException("AccountService needs to be setup!");
             this._contentService = contentService;
         }
-
+        
         public IActionResult OnGet() {
             if ( !this.User.Identity.IsAuthenticated )
                 return this.RedirectToPage("/Index");
@@ -40,32 +37,31 @@ namespace Illusive.Pages {
         }
         
         public async Task<IActionResult> OnPostUploadAsync() {
+            if ( !this.ModelState.IsValid )
+                return this.Page();
+
             var accountId = this.User.FindFirst(ClaimTypes.PrimarySid)?.Value;
-            
-            var file = this.FileUpload;
-            if ( file != null && file.Length > 0 ) {
-                if ( !ImageExtensions.Contains(Path.GetExtension(file.FileName)?.ToUpperInvariant()) ) {
-                    this.ModelState.AddModelError("",
-                        $"Wrong file-type provided! Please use the file types {ImageExtensions.Aggregate((a, b) => $"{a}, {b}")}");
-                    return this.Page();
-                }
-                if ( file.Length > MaxFileSizeBytes ) {
-                    this.ModelState.AddModelError("",
-                        $"The file provided exceeds the maximum size of {MaxFileSizeBytes / 1000 / 1000}MB!");
-                    return this.Page();
-                }
-                
-                var filePath = Path.GetTempFileName();
+            var accountUpdate = this.AccountUpdate ?? throw new NullReferenceException("Account Change data shouldn't be null!");
 
-                await using var stream = System.IO.File.Create(filePath, (int) file.Length);
-                await file.CopyToAsync(stream);
-
-                var path = await this._contentService.UploadFileAsync(Path.GetFileName(file.FileName), stream);
-                
-                var update = Builders<AccountData>.Update.Set(x => x.ProfilePicture, path);
+            var profileBio = accountUpdate.Bio;
+            if ( !string.IsNullOrEmpty(profileBio) ) {
+                var update = Builders<AccountData>.Update.Set(x => x.Bio, profileBio);
                 this._accountService.UpdateAccount(accountId, update);
             }
             
+            var profilePic = accountUpdate.ProfilePicture;
+            if ( profilePic != null && profilePic.Length > 0 ) {
+                var filePath = Path.GetTempFileName();
+
+                await using var stream = System.IO.File.Create(filePath, (int) profilePic.Length);
+                await profilePic.CopyToAsync(stream);
+
+                var path = await this._contentService.UploadFileAsync(Path.GetFileName(profilePic.FileName), stream);
+
+                var update = Builders<AccountData>.Update.Set(x => x.ProfilePicture, path);
+                this._accountService.UpdateAccount(accountId, update);
+            }
+
             return this.Page();
         }
     }
