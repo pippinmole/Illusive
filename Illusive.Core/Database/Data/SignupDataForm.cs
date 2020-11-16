1eca980791731bf9460fb518e770a25b49f1c1bd
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2020-11-16 23:39:18 +0000

    Fixed usernames accepting non-unicode characters

--- Illusive.Core/Database/Data/SignupDataForm.cs
+++ Illusive.Core/Database/Data/SignupDataForm.cs
@@ -1,15 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Illusive.Attributes;
 
 namespace Illusive.Models {
     public class SignupDataForm {
             
-        [Required, MaxLength(15)]
+        [Required, MaxLength(15), UnicodeOnly]
         public string Username { get; set; }
             
-        [Required, DataType(DataType.EmailAddress)]
+        [Required, DataType(DataType.EmailAddress), UnicodeOnly]
         public string Email { get; set; }
             
-        [Required, StringLength(32), DataType(DataType.Password)]
+        [Required, StringLength(32), DataType(DataType.Password), UnicodeOnly]
         public string Password { get; set; }
     }
 }
