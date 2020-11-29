3b23f89dbb888ab2059f7401a88d36ada220b4b9
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2020-11-29 17:26:33 +0000

    Forum API Controller

---
+++ Controllers/ForumController.cs
@@ -0,0 +1,23 @@
+﻿using Illusive.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Illusive.Controllers {
+    [ApiController]
+    [Route("api/v1/forum")]
+    public class ForumController : Controller {
+        private readonly IForumService _forumService;
+
+        public ForumController(IForumService forumService) {
+            this._forumService = forumService;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetForumPost(string id) {
+            var result = this._forumService.GetForumWhere(x => x.Id == id);
+
+            return this.View("Account");            
+            
+            return new JsonResult(result);
+        }
+    }
+}
